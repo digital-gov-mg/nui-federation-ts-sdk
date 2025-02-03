1a7d3af535a8d3be7f08e5fa48e5b160e46e4310
Ando <hi@andostronaut.com>
2025-02-03 14:17:06 +0300

    feat: update sdk structure and update auto pr branch request

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,1 @@
-import { UIN } from './features/uin'
-
-export default class NuiFederation {
-  public uin: UIN
-
-  constructor(
-    baseUrl: string,
-    apiKey: string,
-    clientId: string,
-    clientSecret: string,
-  ) {
-    this.uin = new UIN(baseUrl, apiKey, clientId, clientSecret)
-  }
-}
+export * from './sdk'
---
+++ src/sdk/uin.ts
@@ -0,0 +1,37 @@
+import { HTTP } from '../transports'
+import {
+  GetOrCreateUINRequest,
+  UINResponse,
+  GenerateBatchUINResponse,
+} from '../types'
+
+export class Uin extends HTTP {
+  async getOrCreateUIN(payload: GetOrCreateUINRequest[]): Promise<UINResponse> {
+    return this.request<UINResponse>({
+      url: '/uins',
+      options: {
+        method: 'PUT',
+        data: payload,
+      },
+    })
+  }
+
+  async revokeUIN(uin: string): Promise<UINResponse> {
+    return this.request<UINResponse>({
+      url: `/uins/${uin}/revoke`,
+      options: {
+        method: 'DELETE',
+      },
+    })
+  }
+
+  async generateBatchUIN(count: string): Promise<GenerateBatchUINResponse> {
+    return this.request<GenerateBatchUINResponse>({
+      url: '/uins/batch',
+      options: {
+        method: 'POST',
+        params: { count },
+      },
+    })
+  }
+}
